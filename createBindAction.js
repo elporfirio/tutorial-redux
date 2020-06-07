439be1fc789b55091540453312f28a7f3c5cbaca
Porfirio Chávez González <elporfirio@MacBook-Pro-de-Porfirio.local>
2020-06-07 00:32:41 -0500

    Update bind Action Creators

--- createBindAction.js
+++ createBindAction.js
@@ -1,9 +1,12 @@
 import redux from "redux";
 const { bindActionCreators, createStore } = redux;
 
-const reducer = (state = { phrase: "hello World" }, action) => {
+const reducer = (state = { phrase: "hello World", amount: 0 }, action) => {
   if (action.type === "UPDATE_TYPE") {
     return { ...state, phrase: action.payload };
+  }
+  if (action.type === "ADD") {
+    return { ...state, amount: state.amount + action.payload.amount };
   }
   return state;
 };
@@ -12,15 +15,26 @@ const store = createStore(reducer);
 
 const createAddAction = (amount) => {
   return {
-    type: 'ADD',
+    type: "ADD",
     payload: {
       amount,
-    }
-  }
+    },
+  };
 };
 
+store.subscribe(() => console.log("Se ha movido algo"));
+
+// esto se hace normalmente para hacer el dispatch de un action
 store.dispatch(createAddAction(5));
 
-const dispatchAdd = bindActionCreators(createAddAction, store.dispatch)
+// esto se utiliza cuando se tiene un componente que no quiere saber nada de redux
+// asi que en realidad ni se utiliza
+const dispatchAdd = bindActionCreators(createAddAction, store.dispatch);
 
-console.log(dispatchAdd);
+console.log(dispatchAdd.toString());
+
+// mas info aqui
+dispatchAdd(10);
+
+console.log("GET STATE");
+console.log(store.getState());
